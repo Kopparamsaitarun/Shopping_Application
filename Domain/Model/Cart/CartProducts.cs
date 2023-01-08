3c852252bdbb28c5c952ab160e66fa8ded7c9820
Sangeeth Sudhakaran <Sangeeth.Sudhakaran@marlabs.com>
2023-01-08 12:26:11 +0530

    Modified Cart Model and Repository with Sonal. Only th Product count will store in this model

--- Domain/Model/Cart/CartProducts.cs
+++ Domain/Model/Cart/CartProducts.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks; 
-
-namespace Domain.Model.Cart
+﻿namespace Domain.Model.Cart
 {
     public class CartProducts : BaseEntity
     {
-        public int userId { get; set; }
-        public string productId { get; set; }
-        public int Count { get; set; }
-        public System.DateTime DateCreated { get; set; }
-        public Product.Product product { get; set; }
-        public User.User User { get; set; }
+        public Product.Product product { get; set; }//ProductId
+        public User.User User { get; set; }//UserId
+        public int Count { get; set; }//Increase Decrease
     }
 }
